9a3b24f0631d1a931c091463f9b95ee4eb2b0f80
Paweł <pawel.wankowski@wp.pl>
2021-09-10 20:34:18 +0200

    Utworzenie Controlera Experience oraz metody GET z endpointem /experience

--- src/main/java/com/example/contoller/Experience.java
+++ src/main/java/com/example/contoller/Experience.java
@@ -1,4 +1,14 @@
 package com.example.contoller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class Experience {
+
+    @RequestMapping(value = "/experience", method = RequestMethod.GET)
+    public String getExperience(){
+        return "resume";
+    }
 }
