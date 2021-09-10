9bc4341b5e91b10bde24faff2204256f100139b6
Paweł <pawel.wankowski@wp.pl>
2021-09-10 22:39:47 +0200

    Dodanie Repozytorium SkillRepository dziedziczące po JPARepository  z metodami findall oraz save

---
+++ src/main/java/com/example/repository/SkillRepository.java
@@ -0,0 +1,17 @@
+package com.example.repository;
+
+import com.example.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+    List<Skill> findAll();
+    Skill save(Skill skillEntity);
+
+
+
+}
