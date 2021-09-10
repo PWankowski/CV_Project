51d1ca7813814c3b42a2aa01e81152f273804e83
Paweł <pawel.wankowski@wp.pl>
2021-09-10 20:21:20 +0200

    Utworzenie Controlera Portfolio oraz metody GET z endpointem /portfolio

---
+++ src/main/java/com/example/contoller/Portfolio.java
@@ -0,0 +1,15 @@
+package com.example.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Portfolio {
+
+    @RequestMapping(value = "/portfolio", method = RequestMethod.GET)
+    public String getPortfolio(){
+        return "portfolio/portfolio";
+    }
+}
