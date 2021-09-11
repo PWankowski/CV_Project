999723103a2e163bee63ee73de5d942e7161fd57
Paweł <pawel.wankowski@wp.pl>
2021-09-11 12:03:09 +0200

    Dodanie Kolejnych metod GET dla widoków  dodawania oraz edycji umiejętności oraz dodanie metody POST dla dodania nowej umiejętności

--- src/main/java/com/example/contoller/AboutMe.java
+++ src/main/java/com/example/contoller/AboutMe.java
@@ -1,16 +1,50 @@
 package com.example.contoller;
 
+import com.example.model.Skill;
+import com.example.repository.SkillRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class AboutMe {
 
+    private final SkillRepository skillRepository;
+
+    public AboutMe(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
 
     @RequestMapping(value = "/about", method = RequestMethod.GET)
-    public String getAboutMe(){
+    public String getAboutMe(Model model){
+        List<Skill> skillList = skillRepository.findAll();
+        model.addAttribute("skill", skillList);
         return "about/about";
     }
+
+    @RequestMapping(value = "/addSkills", method = RequestMethod.GET)
+    public String getAddSkills(Model model){
+        List<Skill> skillList = skillRepository.findAll();
+        model.addAttribute("skill", skillList);
+        return "about/addSkills";
+    }
+
+    @RequestMapping(value = "/editSkills", method = RequestMethod.GET)
+    public String getEditSkills(){
+        return "about/editSkills";
+    }
+
+    @RequestMapping(value = "/addSkills", method = RequestMethod.POST)
+    public RedirectView postAddSkills(@ModelAttribute Skill newSkill){
+        skillRepository.save(newSkill);
+        return new RedirectView("/about");
+    }
+
 
 }
